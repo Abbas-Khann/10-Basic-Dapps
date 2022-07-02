9377f9f797e000cadb147d76666ee162e48ee036
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-02 17:12:30 +0430

    Added final touches and comments and wrapped up the dApp

--- Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/Components/input.js
+++ Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/Components/input.js
@@ -16,7 +16,7 @@ const Input = (props) => {
   const storeNFT = async (blob) => {
     // create a new NFTStorage client using our API key
     const nftstorage = new NFTStorage({ token: NFT_KEY });
-    const description = "This is a beautiful and sweet memory of yours ";
+    const description = "This is your beautiful NFT";
 
     const image = new File([blob], "Sweet Memory");
     // call client.store, passing in the image & metadata
@@ -27,6 +27,7 @@ const Input = (props) => {
     });
 
     console.log(token.url);
+    // setting the MetaDataURL to the url returned from the token object
     setMetaDataURL(token.url);
     return token.url;
   };
@@ -35,6 +36,7 @@ const Input = (props) => {
   let formData = new FormData();
 
   const onFileChange = (e) => {
+    // for testing purposes
     console.log("Image Uploaded", e.target.files[0]);
     toast.success("Image Uploaded, Submit it now!");
     if (e.target && e.target.files[0]) {
@@ -51,19 +53,12 @@ const Input = (props) => {
     console.log("form data", formData);
     toast.success("Wait for the promise to be resolved");
     const token = storeNFT(img);
-    // Axios.post('https://v2.convertapi.com/upload',
-    // { formData }
-    // )
-    // .then(res => {
-    //   console.log(res)
-    // })
-    // .catch(err => {
-    //   console.error(err)
-    // })
+    return token
   };
 
   return (
     <div className="w-11/12 mx-auto mb-4">
+      {/* If you want to add the description and the name manually by yourself you can uncomment this code and set it up instead of hardcoding it like me */}
       {/* <input 
      className="text-xl p-1 m-2 rounded ml-12 text-center"
      type="text" id="name" placeholder='Name Your NFT'/>
