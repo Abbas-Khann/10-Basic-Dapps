c1f5e045edff8d112504f5b1e0cd17e4cdb34c11
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-02 10:46:57 +0430

    Added finishing touches

--- Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/Components/input.js
+++ Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/Components/input.js
@@ -1,57 +1,55 @@
-import React, {useState} from 'react';
-import { NFTStorage, File } from 'nft.storage';
-import { NFT_KEY } from '../../env';
-// import { toast } from 'react-toastify';
-import Axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { NFT_KEY } from "../../env";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Input = (props) => {
-  const {setMetaDataURL, setTrackImage, trackImage} = props
-  const [img, setImg] = useState("")
-    /**
-  *
-  * @param {string} image path to an image file
-  * @param {string} name a name for the NFT
-  * @param {string} description a text description for the NFT
-  */
-const storeNFT = async(blob) => {
+  const { setMetaDataURL, setTrackImage, trackImage } = props;
+  const [img, setImg] = useState("");
+  /**
+   *
+   * @param {string} image path to an image file
+   * @param {string} name a name for the NFT
+   * @param {string} description a text description for the NFT
+   */
+  const storeNFT = async (blob) => {
     // create a new NFTStorage client using our API key
-    const nftstorage = new NFTStorage({ token: NFT_KEY })
-    const description = "This is a beautiful and sweet memory of yours "
+    const nftstorage = new NFTStorage({ token: NFT_KEY });
+    const description = "This is a beautiful and sweet memory of yours ";
 
-    const image = new File([blob],"Sweet Memory")
+    const image = new File([blob], "Sweet Memory");
     // call client.store, passing in the image & metadata
     const token = await nftstorage.store({
-        image,
-        name,
-        description,
-    })
+      image,
+      name,
+      description,
+    });
 
     console.log(token.url);
-    setMetaDataURL(token.url) ;
-    return token.url ;
-}
-  
-  var FormData = require('form-data');
+    setMetaDataURL(token.url);
+    return token.url;
+  };
+
+  var FormData = require("form-data");
   let formData = new FormData();
 
   const onFileChange = (e) => {
-    console.log("Image Uploaded", e.target.files[0])
-    toast.success("Image Uploaded, Submit it now!")
-    if(e.target && e.target.files[0]) {
-      formData.append('file', e.target.files[0])
-      setTrackImage(true)
+    console.log("Image Uploaded", e.target.files[0]);
+    toast.success("Image Uploaded, Submit it now!");
+    if (e.target && e.target.files[0]) {
+      formData.append("file", e.target.files[0]);
+      setTrackImage(true);
     }
-    console.log("onfilechange", formData)
+    console.log("onfilechange", formData);
     const [file] = e.target.files;
-    console.log("image here", URL.createObjectURL(file)) ;
-    setImg(URL.createObjectURL(file))
-  }
+    console.log("image here", URL.createObjectURL(file));
+    setImg(URL.createObjectURL(file));
+  };
 
   const submitFileData = () => {
     console.log("form data", formData);
-    toast.success('Wait for the promise to be resolved');
+    toast.success("Wait for the promise to be resolved");
     const token = storeNFT(img);
     // Axios.post('https://v2.convertapi.com/upload',
     // { formData }
@@ -62,33 +60,34 @@ const storeNFT = async(blob) => {
     // .catch(err => {
     //   console.error(err)
     // })
-  }
+  };
 
- return (
-    <div
-    className='w-11/12 mx-auto mb-4'
-    >
-    
-    {/* <input 
+  return (
+    <div className="w-11/12 mx-auto mb-4">
+      {/* <input 
      className="text-xl p-1 m-2 rounded ml-12 text-center"
      type="text" id="name" placeholder='Name Your NFT'/>
     <input className='text-xl p-1 m-2 rounded text-center'
      type="text" id="description" placeholder="Describe Your NFT" />  */}
-    <input 
-    className='ml-12 mb-4'
-    type="file" name='image' onChange={onFileChange}
-    
-    />
-    {/* <p>{metadata}</p> */}
-    <img src={img} alt="" />
-    {trackImage && <button
-    className='inline-block px-12 py-2.5 bg-sky-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out my-2 ml-12'
-    onClick={submitFileData}
-    >
-    Submit</button>}
-    <ToastContainer />
+      <input
+        className="ml-12 mb-4"
+        type="file"
+        name="image"
+        onChange={onFileChange}
+      />
+      {/* <p>{metadata}</p> */}
+      <img src={img} alt="" />
+      {trackImage && (
+        <button
+          className="inline-block px-12 py-2.5 bg-sky-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out my-2 ml-12"
+          onClick={submitFileData}
+        >
+          Submit
+        </button>
+      )}
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/index.js
+++ Web3-Projects/10 Dapps /5) NFT Minter-dApp/frontend/pages/index.js
@@ -1,52 +1,66 @@
-import React, {useState} from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import Navbar from './Components/Navbar'
-import styles from '../styles/Home.module.css'
-import Input from "./Components/Input"
-import Button from './Components/Button';
-import Confetti from 'react-confetti'
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Navbar from "./Components/Navbar";
+import styles from "../styles/Home.module.css";
+import Input from "./Components/Input";
+import Button from "./Components/Button";
+import Confetti from "react-confetti";
 
 const Home = () => {
   const [darkMode, setDarkMode] = useState(false);
-  const [metaDataURL,setMetaDataURL] = useState("");
-  const [trackImage, setTrackImage] = useState(false)
+  const [metaDataURL, setMetaDataURL] = useState("");
+  const [trackImage, setTrackImage] = useState(false);
   const [minted, setMinted] = useState(false);
-  
-  const toggleDarkMode = () => {
-    setDarkMode(prevMode => !prevMode)
-  }
 
-    
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
-    <div className={`${darkMode ? `bg-[#10172a]` : `bg-gradient-to-l from-sky-500 to-emerald-200`} flex flex-col items-center justify-start gap-2 ${trackImage ? `h-fit` : `h-screen`}`} >
-      <Navbar 
-      darkMode={darkMode}
-      toggleDarkMode={toggleDarkMode}
-      />
+    <div
+      className={`${
+        darkMode
+          ? `bg-[#10172a]`
+          : `bg-gradient-to-l from-sky-500 to-emerald-200`
+      } flex flex-col items-center justify-start gap-2 ${
+        trackImage ? `h-fit` : `h-screen`
+      }`}
+    >
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <ToastContainer />
-      <span 
-      className={`text-2xl my-3 ${darkMode ? `text-white` : `text-black` }`}
-      >Store your Memories in a Decentralized Manner</span>
-      <div className={`${styles.cards} flex flex-col mx-1.5 py-1.5 w-2/4 h-3/5`}>
-        <div className={styles.logo}><span className={styles.log1} >NFT</span><span className={styles.log2}>HUB</span> </div>
-      {minted &&
-      <a
-      className="text-lg font-medium  text-white cursor-pointer underline"
-      href="https://testnets.opensea.io/collection/khannft">
-        Here is your NFT ➡️
-      </a> 
-      }
-      {minted && <Confetti />}
-      <Input
-      setMetaDataURL={setMetaDataURL}
-      setTrackImage={setTrackImage}
-      trackImage={trackImage}
-      />
-      {trackImage && <Button metaDataURL={metaDataURL} setMinted={setMinted} />}
-      <ToastContainer />
+      <span
+        className={`text-2xl my-3 ${darkMode ? `text-white` : `text-black`}`}
+      >
+        Store your Memories in a Decentralized Manner
+      </span>
+      <div
+        className={`${styles.cards} flex flex-col mx-1.5 py-1.5 w-2/4 h-3/5`}
+      >
+        <div className={styles.logo}>
+          <span className={styles.log1}>NFT</span>
+          <span className={styles.log2}>HUB</span>{" "}
+        </div>
+        {minted && (
+          <a
+            className="text-lg font-medium  text-white cursor-pointer underline"
+            href="https://testnets.opensea.io/collection/khannft"
+          >
+            Here is your NFT ➡️
+          </a>
+        )}
+        {minted && <Confetti />}
+        <Input
+          setMetaDataURL={setMetaDataURL}
+          setTrackImage={setTrackImage}
+          trackImage={trackImage}
+        />
+        {trackImage && (
+          <Button metaDataURL={metaDataURL} setMinted={setMinted} />
+        )}
+        <ToastContainer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
