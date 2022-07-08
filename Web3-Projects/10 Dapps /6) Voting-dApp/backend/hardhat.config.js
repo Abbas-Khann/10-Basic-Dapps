4c0cedf4a199392242bc950321e966ceac9a9f46
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-08 20:17:59 +0430

    Contract deployed

--- Web3-Projects/10 Dapps /6) Voting-dApp/backend/hardhat.config.js
+++ Web3-Projects/10 Dapps /6) Voting-dApp/backend/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config({path: "./.env"});
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+  mumbai: {
+    url: ALCHEMY_API_KEY_URL,
+    accounts: [MUMBAI_PRIVATE_KEY]
+  }
+  }
 };
