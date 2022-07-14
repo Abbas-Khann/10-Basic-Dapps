cf63f273ecca480c9a70255cbc68587336faf6be
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-14 15:16:31 +0430

    Added tailwind css

---
+++ Web3-Projects/10 Dapps /7) Land Registry-dApp/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'dark',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
