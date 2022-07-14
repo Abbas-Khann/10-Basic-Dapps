863e8c506c044c4034d5178d40c7abc712cf3c98
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-14 15:10:36 +0430

    Frontend Setup

---
+++ Web3-Projects/10 Dapps /7) Land Registry-dApp/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../src/Components/Navbar"
+
+const Home = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>    
+  )
+}
+
+export default Home
