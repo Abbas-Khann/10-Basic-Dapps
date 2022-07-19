43bd364e9614c464bb838070046053cea73194ad
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-19 16:26:20 +0430

    Smart contract deployed

---
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/backend/contracts/JobBoard.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Unlicense
+
+/* Requirements for our Smart Contract
+-> A job should consist of many properties and should be stored inside of a struct
+-> We should be able to add new Jobs to the Job Board
+-> We should be able to see all the available jobs 
+-> We should be able to see the amount of jobs posted
+*/
+pragma solidity ^ 0.8.0;
+
+contract JobBoard {
+    uint256 public JOB_ID = 0;
+    struct Job {
+        string title;
+        string companyName;
+        string description;
+        string employmentType;
+        string location;
+        uint salary;
+        string applyUrl;
+        string contactEmail;
+    }
+
+    Job[] public jobsArray;
+    // mapping (uint => Job) public jobMap;
+
+    function addJob(
+        string memory _title,
+        string memory _companyName,
+        string memory _description,
+        string memory _employmentType,
+        string memory _location,
+        uint _salary,
+        string memory _applyUrl,
+        string memory _contactEmail
+    )
+    public {
+        jobsArray.push(
+            Job(
+             _title,
+            _companyName, 
+            _description, 
+            _employmentType, 
+            _location, 
+            _salary, 
+            _applyUrl, 
+            _contactEmail)
+        );
+    }
+
+    function getJobs() public view returns(Job[] memory) {
+        return jobsArray;
+    }
+
+    function getAllJobsLength() public view returns(uint) {
+        return jobsArray.length;
+    }
+
+    function deleteJob(uint _index) public {
+        jobsArray[_index] = jobsArray[jobsArray.length - 1];
+        jobsArray.pop();
+    }
+
+
+}
--- Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/backend/scripts/deploy.js
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/backend/scripts/deploy.js
@@ -1,24 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat")
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main =  async () => {
+  const jobBoardContract = await ethers.getContractFactory("JobBoard");
+  
+  const deployedJobBoardContract = await jobBoardContract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await deployedJobBoardContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("JobBoard Contract :", deployedJobBoardContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
