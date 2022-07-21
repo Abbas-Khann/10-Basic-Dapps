0018149c98d7380ae1161c8baad7b9c3a93ee855
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-21 09:18:57 +0430

    DarkMode added

---
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/Constants/constants.js
@@ -0,0 +1,200 @@
+export const JOB_BOARD_CONTRACT_ADDRESS = "0x4Ce6F19d3486F5bE4720491eaA4b65681EF6133C";
+
+export const JOB_BOARD_CONTRACT_ABI = [
+    {
+      "inputs": [],
+      "name": "JOB_ID",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_title",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_companyName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_description",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_employmentType",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_location",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_salary",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_applyUrl",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_contactEmail",
+          "type": "string"
+        }
+      ],
+      "name": "addJob",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_index",
+          "type": "uint256"
+        }
+      ],
+      "name": "deleteJob",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAllJobsLength",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getJobs",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "title",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "companyName",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "description",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "employmentType",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "location",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "salary",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "applyUrl",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "contactEmail",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct JobBoard.Job[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "jobsArray",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "title",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "companyName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "employmentType",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "location",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "salary",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "applyUrl",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "contactEmail",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
--- Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/pages/index.js
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/pages/index.js
@@ -2,9 +2,14 @@ import React,{useState} from "react";
 import Navbar from "./Components/navbar";
 
 const Home = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
   return (
     <div>
-      <Navbar />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
     </div>
   )
 }
