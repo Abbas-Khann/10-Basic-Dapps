dcc02410a8417e96ef397788d4dee36668eb6175
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-22 13:22:16 +0430

    Contract Interaction

--- Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/Context/Context.js
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/Context/Context.js
@@ -1,18 +1,55 @@
 import React,{useState, useContext, createContext} from "react";
+import { useContract, useProvider, useSigner } from "wagmi";
+import { JOB_BOARD_CONTRACT_ADDRESS, JOB_BOARD_CONTRACT_ABI } from "../Constants/constants";
 
 export const IndexContext = createContext();
 
 const IndexProvider= ( {children} ) => {
 
-    
+    // Fetching the provider and signer from Wagmi
+    const provider = useProvider();
+    const {data: signer} = useSigner();
+    const contract = useContract({
+        addressOrName: JOB_BOARD_CONTRACT_ADDRESS,
+        contractInterface: JOB_BOARD_CONTRACT_ABI,
+        signerOrProvider: signer || provider
+    });
+
+    // States here
     const [darkMode, setDarkMode] = useState(false);
+    const [jobData, setJobData] = useState({
+        title: "",
+        CompanyName: "",
+        JobDescription: "",
+        EmploymentType: "",
+        JobLocation: "",
+        SalaryRange: 0,
+        OrganisationUrl: "",
+        ContactEmail: ""
+    });
+
+
+
+
+
+
+
+
     
+    const handleInputData = event => {
+        setJobData((prev) => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.value,
+            };
+        });
+    }
+    console.log(jobData) 
     const toggleDarkMode = () => {
         setDarkMode(!darkMode)
     }
-    
     return(
-        <IndexContext.Provider value={ {toggleDarkMode, darkMode} }>
+        <IndexContext.Provider value={ {toggleDarkMode, darkMode, handleInputData} }>
         {children}
         </IndexContext.Provider>
 )
--- Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/pages/CreateNewJob.js
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/pages/CreateNewJob.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { darkMode } from '../tailwind.config'
 import Navbar from './Components/navbar'
 import { useGlobalContext } from '../Context/Context'
 
 const CreateNewJob = () => {
-    const {darkMode} = useGlobalContext();
+    const {darkMode, handleInputData} = useGlobalContext();
   return (
     <main className={`${darkMode && 'dark'}`}>
         <Navbar />
@@ -17,21 +16,21 @@ const CreateNewJob = () => {
         
             <div className='bg-sky-0 flex flex-col p-12 sm:py-4 sm:px-44 lg:px-72'>
                 <h1 className='text-xl '>Enter your Job title<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none  dark:text-black bg-[#F1F6F9]' placeholder='Job title...' />
+                <input onChange={handleInputData} name="title" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none  dark:text-black bg-[#F1F6F9]' placeholder='Job title...' />
                 <h1 className='text-xl '>Enter your Company Name<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Company Name...' />
+                <input onChange={handleInputData} name="CompanyName" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Company Name...' />
                 <h1 className='text-xl '>Enter the Job Description and requirements<span className='text-red-400'>*</span></h1>
-                <textarea className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Job title...' />
+                <textarea onChange={handleInputData} name="JobDescription" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Job title...' />
                 <h1 className='text-xl '>Enter the Employment Type<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Part Time, full time etc...' />
+                <input onChange={handleInputData} name="EmploymentType" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Part Time, full time etc...' />
                 <h1 className='text-xl '>Enter your Job Location<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Location...' />
+                <input onChange={handleInputData} name="JobLocation" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Location...' />
                 <h1 className='text-xl '>Enter the Salary Range<span className='text-red-400'>*</span></h1>
-                <input type="number" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Salary....' />
+                <input onChange={handleInputData} type="number" name='SalaryRange' className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Salary....' />
                 <h1 className='text-xl '>Enter your Organization Url<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Website etc...' />
+                <input onChange={handleInputData} name="OrganisationUrl" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Website etc...' />
                 <h1 className='text-xl '>Enter your Contact Email<span className='text-red-400'>*</span></h1>
-                <input className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Company Email...' />
+                <input onChange={handleInputData} name="ContactEmail" className='border-solid border-2 border-zinc-200 py-1 text-lg rounded px-2 my-4 active:border-sky-300 focus:border-sky-300 outline-none dark:text-black bg-[#F1F6F9]' placeholder='Company Email...' />
 
         </div>
         </section>
