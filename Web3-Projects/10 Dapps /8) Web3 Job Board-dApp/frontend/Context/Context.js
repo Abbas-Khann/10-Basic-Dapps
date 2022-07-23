af7b67daec0a91b58944b4876bc7355f970921a6
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-23 15:06:33 +0430

    Added prettier

--- Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/Context/Context.js
+++ Web3-Projects/10 Dapps /8) Web3 Job Board-dApp/frontend/Context/Context.js
@@ -1,27 +1,25 @@
-import React,{useState, useContext, createContext} from "react";
-import { useContract, useProvider, useSigner } from "wagmi";
-import { JOB_BOARD_CONTRACT_ADDRESS, JOB_BOARD_CONTRACT_ABI } from "../Constants/constants";
+import React, { useState, useContext, createContext } from "react";
 
 export const IndexContext = createContext();
 
-const IndexProvider = ( {children} ) => {
-    
-    // States here
-    const [darkMode, setDarkMode] = useState(true);
-    const [indexData , setIndexData] = useState('');
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode)
-    }
-    return(
-        <IndexContext.Provider value={ {toggleDarkMode, darkMode,  indexData , setIndexData} }>
-        {children}
-        </IndexContext.Provider>
-)
-
-}
+const IndexProvider = ({ children }) => {
+  // States here
+  const [darkMode, setDarkMode] = useState(true);
+  const [indexData, setIndexData] = useState("");
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  return (
+    <IndexContext.Provider
+      value={{ toggleDarkMode, darkMode, indexData, setIndexData }}
+    >
+      {children}
+    </IndexContext.Provider>
+  );
+};
 
 export const useGlobalContext = () => {
-    return useContext(IndexContext)
-}
+  return useContext(IndexContext);
+};
 
-export { IndexProvider }
+export { IndexProvider };
