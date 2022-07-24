49fafe139a3d9b1596d4627278d2a27e92022d53
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-24 22:53:03 +0430

    Backend Setup

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/backend/scripts/deploy.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/backend/scripts/deploy.js
@@ -1,24 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main = async () => {
+  
+  const healthcareContract = await ethers.getContractFactory("Healthcare");
+  
+  const deployedHealthcareContract = await healthcareContract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await deployedHealthcareContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(deployedHealthcareContract.address);
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
