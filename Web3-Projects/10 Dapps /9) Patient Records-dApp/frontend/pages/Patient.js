24d0a658ac6d5bf3d52086d15cf6e8388d6a2e89
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-25 20:20:49 +0430

    Patient Page completed

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
@@ -10,9 +10,10 @@ const Patient = () => {
   return (
     <section className="h-screen bg-cover bg-[url('/img/doctorr.jpeg')] lg:bg-[url('/img/doctor.jpeg')]">
         <div className='absolute inset-0 bg-gray-900 bg-opacity-60'>
-            
+            <h1 className='text-center text-white text-3xl mt-4'>Abbas Khan</h1>
+            <div className=''>
+            </div>
         </div>
-        <h1>Patient Name</h1>
     </section>
   )
 }
