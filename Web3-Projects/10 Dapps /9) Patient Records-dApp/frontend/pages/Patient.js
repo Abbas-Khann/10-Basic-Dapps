7e377c17b5c363afd56736fa964c11f655299499
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-25 20:15:13 +0430

    Patient page created

---
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Patient = () => {
+
+
+
+
+
+
+  return (
+    <section className="h-screen bg-cover bg-[url('/img/doctorr.jpeg')] lg:bg-[url('/img/doctor.jpeg')]">
+        <div className='absolute inset-0 bg-gray-900 bg-opacity-60'>
+            
+        </div>
+        <h1>Patient Name</h1>
+    </section>
+  )
+}
+
+export default Patient
