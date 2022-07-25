67f1596b4cfa18d9d96befa8bca09d9605dcc90b
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-25 20:25:13 +0430

    Patient page done

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Patient = () => {
 
-
-
-
-
+  
 
   return (
+    
     <section className="h-screen bg-cover bg-[url('/img/doctorr.jpeg')] lg:bg-[url('/img/doctor.jpeg')]">
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap');
+        </style>
         <div className='absolute inset-0 bg-gray-900 bg-opacity-60'>
-            <h1 className='text-center text-white text-3xl mt-4'>Abbas Khan</h1>
-            <div className=''>
-            </div>
+            
         </div>
     </section>
   )
