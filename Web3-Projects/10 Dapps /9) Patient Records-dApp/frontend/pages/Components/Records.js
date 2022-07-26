8db8b6122bacb375c2eeb163dfc84642e7a102f7
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-26 15:10:19 +0430

    Patient records component finished

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Records = () => {
   return (
     <div className='flex flex-col justify-center text-white'>
-        <h1 className='text-xl mb-4'>Upload your documents here</h1>
+        <h1 className='text-xl mb-4 '>Upload your documents here</h1>
         <input className='p-2 rounded text-black' placeholder='Enter your File Name'/>
         <input className='bg-white rounded mt-2 text-black' type="file"/>
         <div>
