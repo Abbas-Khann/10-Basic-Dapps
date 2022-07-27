680d59d204a71601079c7085fdc8b09f3c6ae59c
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-27 20:25:42 +0430

    Fetched value

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Records = () => {
+
+  const [fileName, setFileName] = useState('')
+
+  const onChange = event => {
+    setFileName(event.target.value)
+  }
+
+  console.log(fileName)
+
   return (
     <div className='flex flex-col justify-center text-white'>
         <h1 className='text-xl mb-4 '>Upload your documents here</h1>
-        <input className='p-2 rounded text-black' placeholder='Enter your File Name'/>
+        <input onChange={onChange} className='p-2 rounded text-black' placeholder='Enter your File Name'/>
         <input className='bg-white rounded mt-2 text-black' type="file"/>
         <div>
         <button className="bg-cyan-500 hover:bg-cyan-400 rounded px-4 py-2 mt-2 text-white">Upload</button>
