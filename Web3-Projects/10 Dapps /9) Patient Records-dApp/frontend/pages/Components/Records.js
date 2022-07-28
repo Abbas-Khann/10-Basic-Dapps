1bad1018c0943794833175b3d00cbc1748002a60
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-28 09:55:10 +0430

    fileName pushed to ipfs

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Components/Records.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import *  as IPFS  from 'ipfs-core'
 
-const Records = ({onChange, setHash}) => {
+const Records = ({onChange, setHash, fileName}) => {
   // First off i need to push hello world to ipfs for testing purposes
   // Secondly i need to get the data aswell
   // after that i need to set the input box up to the adding data to IPFS and test it by getting the data
@@ -9,22 +9,28 @@ const Records = ({onChange, setHash}) => {
   // const [path, setPath] = useState('')
 
   const addDataIPFS = async () => {
-    const node = await IPFS.create();
-    const data = 'What up dawg!'
-    const result = node.add(data);
-    const resolvedPromise = result.then((promise) => console.log("resolvedPromise:", setHash(promise.path)))
-    console.log(result)
+    try{
+
+      const node = await IPFS.create();
+      const data = fileName;
+      const result = node.add(data);
+      const resolvedPromise = result.then((promise) => console.log("resolvedPromise:", setHash(promise.path)))
+      console.log(result)
+    }
+    catch(err){
+      console.error(err)
+    }
   }
   // getDataIPFS("QmYjtnHhxgaqbSuQW9Xwj2g1EEWJYqaynhnFf6MXHd8SrT")
 
   // console.table(path)
 
 
-  useEffect(() => {
-    addDataIPFS()
-    // getDataIPFS()
-    // testSaveNameToIpfs();
-  }, [])
+  // useEffect(() => {
+  //   addDataIPFS()
+  //   // getDataIPFS()
+  //   // testSaveNameToIpfs();
+  // }, [])
 
 
   return (
@@ -33,7 +39,8 @@ const Records = ({onChange, setHash}) => {
         <input onChange={onChange} className='p-2 rounded text-black' placeholder='Enter your File Name'/>
         <input className='bg-white rounded mt-2 text-black' type="file"/>
         <div>
-        <button 
+        <button
+        onClick={() => addDataIPFS()} 
         className="bg-cyan-500 hover:bg-cyan-400 rounded px-4 py-2 mt-2 text-white">
         Upload
         </button>
