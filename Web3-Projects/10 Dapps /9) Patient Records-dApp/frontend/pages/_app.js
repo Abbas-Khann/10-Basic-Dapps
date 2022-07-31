e58cc801024003d953b1d67cfc949499ea4b1cdf
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-31 10:53:10 +0430

    Added wallet connection

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/_app.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/_app.js
@@ -1,44 +1,33 @@
-import '../styles/globals.css'
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit'; 
-import {
-  chain,
-  configureChains,
-  createClient,
-  WagmiConfig,
-} from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 const { chains, provider } = configureChains(
   [chain.polygonMumbai],
-  [
-    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'Healthcare Records',
-  chains
+  appName: "Healthcare Records",
+  chains,
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
-})
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
   );
 }
 
-export default MyApp
+export default MyApp;
