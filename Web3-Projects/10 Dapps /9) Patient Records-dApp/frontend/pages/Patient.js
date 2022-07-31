c61ec85fee36b224633bfac27f19bf8c1e4326d5
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-07-31 10:52:26 +0430

    Added finishing touches to the patient Page

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/frontend/pages/Patient.js
@@ -1,45 +1,57 @@
-import React, {useState} from 'react'
-import Records from './Components/Records'
-import Table from './Components/Table'
+import React, { useState } from "react";
+import Records from "./Components/Records";
+import Table from "./Components/Table";
 
 const Patient = () => {
-  const [fileName, setFileName] = useState('')
-  const [hash, setHash] = useState('')
+  const [fileName, setFileName] = useState("");
+  const [hash, setHash] = useState("");
   const [selectedFile, setSelectedFile] = useState([]);
 
-  const onChange = event => {
-    setFileName(event.target.value)
-  }
+  const onChange = (event) => {
+    setFileName(event.target.value);
+  };
 
-  console.log("selectedFile", selectedFile)
-  console.log("hash", hash)
+  console.log("selectedFile", selectedFile);
+  console.log("hash", hash);
 
   return (
     <section className="h-screen bg-cover bg-[url('/img/doctorr.jpeg')] lg:bg-[url('/img/doctor.jpeg')]">
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap');
-        </style>
-        <div className='absolute inset-0 bg-gray-900 bg-opacity-80'>
-            <h1 className='text-center text-white text-3xl mt-4'>Your Admin</h1>
-            <div className='sm:flex justify-evenly sm:py-20'>
-            <Table />
-            <Records onChange={onChange} setHash={setHash} fileName={fileName} hash={hash} selectedFile={selectedFile} setSelectedFile={setSelectedFile}/>
-            </div>
-            <div className='flex justify-center items-center'>
-              {hash && (
-                <div className='flex flex-col'>
-                <h1 className='text-2xl text-white text-center my-2'>{fileName}</h1>
-                <a 
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap');
+      </style>
+      <div className="absolute inset-0 bg-gray-900 bg-opacity-80">
+        <h1 className="text-center text-white text-3xl mt-4">Your Admin</h1>
+        <div className="sm:flex justify-evenly sm:py-20">
+          <Table />
+          <Records
+            onChange={onChange}
+            setHash={setHash}
+            fileName={fileName}
+            hash={hash}
+            selectedFile={selectedFile}
+            setSelectedFile={setSelectedFile}
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          {hash && (
+            <div className="flex flex-col">
+              <h1 className="text-2xl text-white text-center my-2">
+                {fileName}
+              </h1>
+              <a
                 href={`ipfs.io/ipfs/${hash}`}
                 target="_blank"
-                className='text-2xl text-blue-400 border-b-sky-500 border-b-2'>Click here to see it in action</a>
-                </div>
-              )}
+                className="text-2xl text-blue-400 border-b-sky-500 border-b-2"
+              >
+                Click here to see it in action
+              </a>
             </div>
+          )}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Patient
+export default Patient;
