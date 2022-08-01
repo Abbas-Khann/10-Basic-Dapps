756acf6c28e3358721abcff24b0b80c9bda510da
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-01 11:59:09 +0430

    Updated Solidity Code

--- Web3-Projects/10 Dapps /9) Patient Records-dApp/backend/contracts/Healthcare.sol
+++ Web3-Projects/10 Dapps /9) Patient Records-dApp/backend/contracts/Healthcare.sol
@@ -52,18 +52,6 @@ contract Healthcare {
      hasRegistered[msg.sender] = true;
      patient_ID++;
     }
-    
-//    function updatePatient(
-//     string memory _name,
-//     uint _age,
-//     string memory _sex, 
-//     string memory _location
-//     ) external {
-//     patientArray[patientMap[msg.sender]].name = _name;
-//     patientArray[patientMap[msg.sender]].age = _age;
-//     patientArray[patientMap[msg.sender]].sex = _sex;
-//     patientArray[patientMap[msg.sender]].location = _location;
-//    }
 
     function addToDocsArr(
         string memory _docName,
